9f6f2ac80e100e4a5fe06e8eede62cbb99a22640
sanjeevreddy <sanjeevreddy644@gmail.com>
2019-07-30 10:58:09 +0530

    integreted barsList service in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,14 +8,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  products: any;
-  addtocart: any;
-  productsForm: FormGroup;
+  cementProducts: any;
+  barsProducts: any;
+  addTocart: any;
 
   constructor(private productService: ProductsService) {}
 
   ngOnInit() {
     this.cementProductsList();
+    this.barsproductsList();
   }
   cementProductsList() {
     this.productService.getCementProducts().subscribe(resp => {
@@ -23,18 +24,45 @@ export class HomeComponent implements OnInit {
         resp.product[i]["qty"] = "";
         resp.product[i]["totalPrice"] = "";
       }
-      this.products = resp.product;
+      this.cementProducts = resp.product;
     });
   }
 
-  cart() {}
-
   productValue(item, quantityEvent, index) {
     let totalPrice = quantityEvent * item.price;
-    this.products[index]["totalPrice"] = totalPrice;
-    this.products[index]["qty"] = quantityEvent;
-    this.addtocart = this.products[index];
-    // console.log(this.catrtValue);
-    console.log("productValue2", JSON.stringify(this.products[index]));
+    this.cementProducts[index]["totalPrice"] = totalPrice;
+    this.cementProducts[index]["qty"] = quantityEvent;
+    this.addTocart = this.cementProducts[index];
+  }
+  barsproductsList() {
+    this.productService.getBarsProducts().subscribe((response: any) => {
+      for (var i = 0; i < response.product.length; i++) {
+        let barsList = response.product[i].barsList;
+
+        for (var j = 0; j < barsList.length; j++) {
+          barsList[j]["quantity"] = "";
+          barsList[j]["totalPrice"] = "";
+          response.product[i]["totalCartPrice"] = "";
+        }
+      }
+      this.barsProducts = response.product;
+    });
+  }
+
+  barsQuntity(bar, quantityEvent, itemIndex, barsIndex) {
+    let totalPrice = quantityEvent * bar.value;
+    let barsProducts = this.barsProducts;
+
+    for (var k = 0; k < barsProducts.length; k++) {
+      var barsList = barsProducts[itemIndex].barsList;
+    
+      for (var m = 0; m < barsList.length; m++) {
+        barsList[barsIndex]["totalPrice"] = totalPrice;
+        barsList[barsIndex]["quantity"] = quantityEvent;
+      }
+    }
+    var totalCartPrice = barsList.reduce((a , b) => a + b.totalPrice,0)
+    barsProducts[itemIndex]["totalCartPrice"] = totalCartPrice;
+
   }
 }
