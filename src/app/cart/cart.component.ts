326e65c5e8e36543d2694419ae07ecb376c3214f
Kiran <kiran@macsof.com>
2019-08-06 17:15:00 +0530

    change cart items values

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -16,6 +16,7 @@ export class CartComponent implements OnInit {
   totBaramount = 0;
   totCementamount = 0;
   totGrandamount = 0;
+  updateCart: any;
 
   constructor(
     private cartService: UserService,
@@ -29,78 +30,70 @@ export class CartComponent implements OnInit {
   }
 
   cartItems() {
+    this.totGrandamount = 0;
     let userId = {
       user_id: this.user.user_id
     };
     this.cartService.userCartList(userId).subscribe(resp => {
       this.cartProducts = resp.result;
-
       for (var j = 0; j < this.cartProducts.length; j++) {
-        let grandTotal = this.cartProducts[j].totalPrice;
-        //console.log("grand", grandTotal);
-        let qty = this.cartProducts[j].quantity;
-        //console.log("qty", qty);
-        let price = this.cartProducts[j].price;
-        let itemTotalPrice = qty * price;
+        let itemTotalPrice = this.cartProducts[j].quantity * this.cartProducts[j].price;
         if(this.cartProducts[j].type_id == 1){
           this.totCementamount = this.totCementamount + itemTotalPrice;
-          console.log("result--->", resp.result[j]);
           resp.result[j]["totalPrice"] = this.totCementamount;
           this.totGrandamount = this.totGrandamount + this.totCementamount;
+          console.log("cement-->", this.totCementamount);
+          this.totCementamount = 0;
         }
-        /* kiran */
         if(this.cartProducts[j].type_id == 2){
           if(this.cartProducts[j].bar_products.length > 0){
             for (let k = 0; k < this.cartProducts[j].bar_products.length; k++) {
                 this.totBaramount = this.totBaramount + (this.cartProducts[j].bar_products[k].quantity *this.cartProducts[j].bar_products[k].price); 
             }
-           // console.log("result--->", resp.result[j]);
             resp.result[j]["barTotal"] =  this.totBaramount;
             this.totGrandamount = this.totGrandamount + this.totBaramount;
+            console.log("bar-->", this.totBaramount);
+            this.totBaramount = 0;
           }
-        }
-        
+        } 
       }
-      /* console.log(this.cartGrandTotal, "grandTotal");
-      localStorage.setItem("cartItem", JSON.stringify(this.cartProducts));
-      console.log("cartProducts", this.cartProducts); */
+      console.log("grand tot-->", this.totGrandamount);
     });
   }
+  
+  chageCartValue(cartItem, bars,  quantity, typeId){
+    /* console.log("cart item--->", cartItem);
+    console.log("cart qty--->", quantity);
+    console.log("bars--->", bars); */
 
-  cartValue(cart, quantity, index) {
-    let cartItem = JSON.parse(localStorage.getItem("cartItem"));
-    console.log(cartItem, "cartItem");
-
-    for (var l = 0; l < cartItem.length; l++) {
-      var typeId = cartItem[index].type_id;
-      var prodId = cartItem[index].prod_type_id;
-      console.log("typeId", typeId);
-    }
-    let price = cart.price;
-    var cartItemPrice = price * quantity;
-    for (var k = 0; k < this.cartProducts.length; k++) {
-      let cartitem = this.cartProducts[k].totalPrice;
-      console.log(cartitem, "cartitem");
-      this.cartProducts[index]["totalPrice"] = cartItemPrice;
-
-      let updateCart = {
+    if(typeId == 1){
+      this.updateCart = {
         user_id: this.user.user_id,
         type_id: typeId,
-        prod_type_id: prodId,
+        prod_type_id: cartItem.prod_type_id,
         quantity: quantity,
         bar_products: []
       };
-      console.log("updatecart", updateCart);
-      this.cartService.addToCart(updateCart).subscribe(resp => {
-        console.log("cartUpdate", resp);
-      });
-
-      this.cartGrandTotal = this.cartProducts.reduce(
-        (a, b) => a + b.totalPrice,
-        0
-      );
-      console.log("grandTotal", this.cartGrandTotal);
+    }else{
+      let bar_products = {
+        "bar_id" : bars.id,
+        "quantity" : quantity
+      }
+      this.updateCart = {
+        user_id: this.user.user_id,
+        type_id: typeId,
+        prod_type_id: cartItem.prod_type_id,
+        quantity: 0,
+        bar_products: [bar_products]
+      };
     }
+    //console.log(this.updateCart);
+     this.cartService.addToCart(this.updateCart).subscribe(resp => {
+      //console.log("cartUpdate", resp);
+      if(resp.status == 200){
+        this.cartItems();
+      }
+    }); 
   }
 
   deleteCarItem(deleteVal, cartVal) {
