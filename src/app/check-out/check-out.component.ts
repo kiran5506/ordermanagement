a56f273d9a9bc79ce5f40909010ef78d72a31543
sanjeevredy644 <sanjeevreddy644@gmail.com>
2019-09-09 18:56:10 -0700

    Resolved error in checkout component

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -29,7 +29,7 @@ export class CheckOutComponent implements OnInit {
   refName: string = "";
   refNumberMessage: string = "";
   refNameMessage: string = "";
-  selectedItem = null ;
+  // selectedItem = null ;
   constructor(
     private userService: UserService,
     private productService: ProductsService,
